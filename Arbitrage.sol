83bd24357d606cd724ef89b3c946318c3afe6d32
doshipm91 <doshipm91@gmail.com>
2026-04-03 21:47:15 +0530

    Refactor executeArbitrage to include token addresses

--- Arbitrage.sol
+++ Arbitrage.sol
@@ -7,24 +7,38 @@ interface IDEX {
     function swapBforA(uint amount) external;
 }
 
+interface IERC20 {
+    function approve(address spender, uint amount) external returns (bool);
+}
+
 contract Arbitrage {
 
-    function executeArbitrage(address dex1, address dex2, uint amount) public {
+    function executeArbitrage(address dex1, address dex2, address tokenA, address tokenB, uint amount) public {
 
+        // Get reserves
         (uint rA1, uint rB1) = IDEX(dex1).getReserves();
         (uint rA2, uint rB2) = IDEX(dex2).getReserves();
 
+        // Prices
         uint price1 = (rB1 * 1e18) / rA1;
         uint price2 = (rB2 * 1e18) / rA2;
 
+        // Approve both DEX
+        IERC20(tokenA).approve(dex1, amount);
+        IERC20(tokenA).approve(dex2, amount);
+        IERC20(tokenB).approve(dex1, amount);
+        IERC20(tokenB).approve(dex2, amount);
+
         if (price1 > price2) {
-            // Buy from dex2 (cheap), sell in dex1 (expensive)
+            // Buy cheap from dex2 → sell in dex1
             IDEX(dex2).swapAforB(amount);
             IDEX(dex1).swapBforA(amount);
         } else if (price2 > price1) {
-            // Buy from dex1, sell in dex2
+            // Buy cheap from dex1 → sell in dex2
             IDEX(dex1).swapAforB(amount);
             IDEX(dex2).swapBforA(amount);
+        } else {
+            revert("No arbitrage opportunity");
         }
     }
 }
